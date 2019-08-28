1d86d7710c28fa2214933184c9d3651ff5019f5b
Goutham Jagga <gouthamjagga@gmail.com>
2019-08-28 23:12:11 +0530

    added routes

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,33 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <b-navbar>
+        <template slot="brand">
+            <b-navbar-item href="/">
+                <img src="./assets/logo.png">
+            </b-navbar-item>
+        </template>
+        <template slot="start">
+            <b-navbar-item href="#">
+                Home
+            </b-navbar-item>
+            <b-navbar-item href="#">
+                Documentation
+            </b-navbar-item>
+        </template>
+
+        <template slot="end">
+            <b-navbar-item tag="div">
+                <div class="buttons">
+                    <a class="button is-primary">
+                        <router-link to="/login"><strong>Sign up</strong></router-link>
+                    </a>
+                    <a class="button is-light">
+                        <router-link to="/login">Login</router-link>
+                    </a>
+                </div>
+            </b-navbar-item>
+        </template>
+    </b-navbar>
     <router-view/>
   </div>
 </template>
@@ -15,17 +39,7 @@
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-}
-#nav {
-  padding: 30px;
+  background-color: rgb(26, 26, 26);
 }
 
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
